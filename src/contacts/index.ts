de379dda6dcc8a370c6b39387f6d2ba3e36a17d7
Daniel Porumb <daniel.porumb@reyno.co.uk>
2019-08-06 09:22:05 +0300

    restructure contact

--- src/contacts/index.ts
+++ src/contacts/index.ts
@@ -1,16 +1,13 @@
 import {Router, RouterConfiguration} from 'aurelia-router';
-import {inject, PLATFORM} from 'aurelia-framework';
-import {WebAPI} from './web-api';
+import {PLATFORM} from 'aurelia-framework';
 
-@inject(WebAPI)
 export class App {
   router: Router;
 
   configureRouter(config: RouterConfiguration, router: Router){
     config.title = 'Contacts';
     config.map([
-      { route: '', moduleId: PLATFORM.moduleName('no-selection'), title: 'Select'},
-      { route: 'contacts',  moduleId: PLATFORM.moduleName('contacts/index'), name:'contacts', nav:true, title: 'Contacts' },
+      { route: ['','list'],moduleId: PLATFORM.moduleName('./list'), title: 'Select'},      
       // { route: 'items',  moduleId: PLATFORM.moduleName('item'), name:'items' }
     ]);
 
