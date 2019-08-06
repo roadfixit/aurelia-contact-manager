f2149c7296e3e6fbc066f7351cb7dd00477ceb66
Stefan Catalin <stefancatalinpaul@gmail.com>
2019-08-06 16:07:01 +0300

    fsjsonsave

--- src/contacts/list.ts
+++ src/contacts/list.ts
@@ -1,13 +1,29 @@
+'use strict';
 import {EventAggregator} from 'aurelia-event-aggregator';
 import {WebAPI} from '../web-api';
 import {ContactUpdates, ContactViewed} from '../messages';
 import {inject, autoinject} from 'aurelia-framework';
 import { Router } from 'aurelia-router';
-  
+
+const fs = require('fs');
+
+let student = { 
+  name: 'Mike',
+  age: 23, 
+  gender: 'Male',
+  department: 'English',
+  car: 'Honda' 
+};
+
+let jsondata = JSON.stringify(student, null, 2);
+  debugger
+
   @autoinject
   export class ContactList {
     contacts;
     selectedId = 0;
+
+
   
     constructor(private router: Router, private api: WebAPI, ea: EventAggregator) { 
       ea.subscribe(ContactViewed, msg => this.select(msg.contact));
@@ -15,6 +31,8 @@ import { Router } from 'aurelia-router';
         let id = msg.contact.id;
         let found = this.contacts.find(x => x.id == id);
         Object.assign(found,msg.contact);
+
+        
       });
     }
   
@@ -25,6 +43,15 @@ import { Router } from 'aurelia-router';
     select(contact) {
       this.selectedId = contact.id;
       return true;
+    }
+//
+    test(){
+
+        fs.writeFile('./jsondata.json', jsondata, (err) => {
+        if (err) throw err;
+        console.log('Data written to file');
+    });
+//
     }
 
     navigateAdd() {
